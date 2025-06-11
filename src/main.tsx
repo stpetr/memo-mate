1361dd08e5d4cf5b1f1f2ea6321d7d28d726708e
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2025-06-11 19:12:21 +0700

    Update to react 19

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 )
